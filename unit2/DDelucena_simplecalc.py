2995917e584c38c226844c43348da0ebbeb1f787
Dylan Delucena <dylandelucena@uagateway.org>
2016-10-05 16:16:53 +0000

    worked on unit 2 lab 1

---
+++ unit2/DDelucena_simplecalc.py
@@ -0,0 +1,15 @@
+user_equation = input("Enter an eqution: ")
+num_1 = user_equation[0]
+operator = user_equation[1]
+num_2 = user_equation[2]
+if:
+    operator == "+":
+
+print ("The result is " + str(equation_entered))
+
+
+#equation_entered = int(user_equation)
+
+#user_input1 = input("Enter your first number: ")
+#user_input2 = input("Enter your operation: ")
+#user_input3 = input("Enter your second number: ")
