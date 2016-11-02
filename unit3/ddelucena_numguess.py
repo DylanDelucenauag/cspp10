a8a1e363475171b1ec538c19dfeb8df82ffe5bc5
Dylan Delucena <dylandelucena@uagateway.org>
2016-11-02 16:15:13 +0000

    Almost done with numguess

--- unit3/ddelucena_numguess.py
+++ unit3/ddelucena_numguess.py
@@ -1,9 +1,24 @@
 import random
+comp_number = random.randint(1,100)
+player_guess = int(input("Guess a number from 1 to 100: "))
 
-while x in range(1):
-    random.randint(1,100)
+while (player_guess != comp_number):
+    if player_guess > comp_number:
+        print ("Too high!")
+        print ("Try again")
+        player_guess = int(input("Guess again: "))
+        
+    elif player_guess < comp_number:
+        print ("Too low!")
+        print ("Try again!")
+        player_guess = int(input("Guess again: "))
+    
+    
+if (player_guess == comp_number):
+    print ("Good Job")
+    print ("The number was " + str(comp_number))
+    print("You're like the best person ever!")
 
-print (x)
 
 
 
