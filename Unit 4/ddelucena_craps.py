49fb349518a88c881b5f9d6f33f740a7e6581bf7
Dylan Delucena <dylandelucena@uagateway.org>
2016-12-19 17:15:40 +0000

    Worked on Craps - need halp

--- Unit 4/ddelucena_craps.py
+++ Unit 4/ddelucena_craps.py
@@ -20,7 +20,9 @@ def roll2dice():
     else:
         print ("Rolled 2 dice: {} and {}, {} in total.".format(dice1,dice2,dice_sum))
         print ("Your point number is: " + str(dice_sum))
-    return dice_sum
+        return dice_sum
+
+
     
 def p1_bet():
     user_bank = 100
@@ -55,10 +57,22 @@ def phase3(dice_sum):
 def craps():
     dice_sum = roll2dice()
     dice_sum_phase3 = phase3(dice_sum)
-    #while dice_sum:
-    phase3(dice_sum)
-#p1_bet()
-#roll2dice()
-#phase3(dice_sum)
+    
+    while dice_sum_phase3 != dice_sum:
+        if dice_sum == "lose":
+            print ("You lost2")
+            break
+        elif dice_sum == "win":
+            print ("You won2")
+            break
+        else:
+            return dice_sum
 
-craps()
+
+
+p1_bet()
+roll2dice()
+dice_sum = roll2dice()
+phase3(dice_sum)
+
+#craps()
