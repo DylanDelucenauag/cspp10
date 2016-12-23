bb2b77a7edc4cb6e5cc020c1d02473c14a478ead
Dylan Delucena <dylandelucena@uagateway.org>
2016-12-23 17:13:04 +0000

    Worked on Craps

--- Unit 4/ddelucena_craps.py
+++ Unit 4/ddelucena_craps.py
@@ -9,16 +9,20 @@ def roll2dice():
     dice2 = random.randint(1,6)
     dice_sum = dice1 + dice2
     if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        print ("Rolled 2 dice: {} and {}, {} in total!".format(dice1,dice2,dice_sum))
         print ("You lost your bet!")
+        
         #return dice_sum
         return "lose"
         
     elif dice_sum == 7 or dice_sum == 11:
+        print ("Rolled 2 dice: {} and {}, {} in total!".format(dice1,dice2,dice_sum))
         print ("You won")
         #return dice_sum
         return "win"
     else:
-        print ("Rolled 2 dice: {} and {}, {} in total.".format(dice1,dice2,dice_sum))
+        print ("Rolled 2 dice: {} and {}, {} in total!".format(dice1,dice2,dice_sum))
+        print ("This will be your point number.")
         return dice_sum
 
 
@@ -42,8 +46,17 @@ def point_number(dice_sum):
     while True:
         dice1 = random.randint(1,6)
         dice2 = random.randint(1,6)
-        dice_sum = dice1 + dice2
-        print ("Rolled 2 dice: {} and {}, {} in total.".format(dice1,dice2,dice_sum))
+        dice_sum_check = dice1 + dice2
+        point_number = dice_sum_check
+        print ("Rolled 2 dice: {} and {}, {} in total.".format(dice1,dice2,dice_sum_check))
+        if dice_sum_check == dice_sum:
+            print ("You win")
+            return "point win"
+        elif dice_sum_check == 7:
+            print ("You lost")
+            return "lose"
+        else:
+            print ("-----------")
 
 
 def phase3(dice_sum):
@@ -62,21 +75,22 @@ def craps():
     dice_sum = roll2dice()
     dice_sum_phase3 = phase3(dice_sum)
     
-    while dice_sum_phase3 != dice_sum:
-        if dice_sum == "lose":
-            print ("You lost2")
-            break
-        elif dice_sum == "win":
-            print ("You won2")
-            break
-        else:
-            return dice_sum
+    
+    if dice_sum == "lose":
+        print ("You lost!")
+        
+    elif dice_sum == "win":
+        print ("You won!")
+        
+    else:
+        return dice_sum
 
 
 
 p1_bet()
-roll2dice()
+#roll2dice()
 dice_sum = roll2dice()
-phase3(dice_sum)
+point_number(dice_sum)
+#phase3(dice_sum)
 
 #craps()
