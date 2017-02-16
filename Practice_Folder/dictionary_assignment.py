44e0bce2d87354bc6f528abd7913e4658033cf77
Dylan Delucena <dylandelucena@uagateway.org>
2017-02-16 17:15:44 +0000

    Worked on add through print function

--- Practice_Folder/dictionary_assignment.py
+++ Practice_Folder/dictionary_assignment.py
@@ -1,13 +1,29 @@
-while True:
-    schedule = {
-        "A":"Algebra 2",
-        "B":"English 10",
-        "C":"Ap Chemistry",
-        "D":"Global",
-        "E":"Global",
-        "FGrey":"Health",
-        "FRed":"Gym"
-    }
-    x = input("Period letter: ")
+import pprint 
+dictionary = {}
+def add():
+    while True:
+        key = input("What key value would you like to add?: ")
+        value = input("What value are you gong to assign this key?: ")
+        if key in dictionary:
+            print ("Sorry that key is already in your dictionary.")
+            add()
+        else:
+            dictionary[key] = key
+            dictionary[value] = value
+    
+        print (dictionary)
+    
 
-    print (schedule[x])
+
+def remove_key():
+    while True:
+        remove_key = input("State the name of the key that you want to remove: ")
+        if remove_key in dictionary:
+            del dictionary[remove_key]
+        else:
+            print("That is not an existing key in your dictionary.")
+            remove_key()
+        
+
+#add()
+remove_key()
