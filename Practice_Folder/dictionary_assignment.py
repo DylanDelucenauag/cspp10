4a4dc4fdf3c7de6a6fc0e68eac5c4270e3724570
Dylan Delucena <dylandelucena@uagateway.org>
2017-02-17 17:15:16 +0000

    dictionary

--- Practice_Folder/dictionary_assignment.py
+++ Practice_Folder/dictionary_assignment.py
@@ -1,15 +1,16 @@
-import pprint 
+from pprint import pprint 
 dictionary = {}
 def add():
-    while True:
         key = input("What key value would you like to add?: ")
         value = input("What value are you gong to assign this key?: ")
         if key in dictionary:
             print ("Sorry that key is already in your dictionary.")
-            add()
+            
         else:
             dictionary[key] = key
             dictionary[value] = value
+            return key, value
+            
     
         print (dictionary)
     
@@ -23,7 +24,25 @@ def remove_key():
         else:
             print("That is not an existing key in your dictionary.")
             remove_key()
-        
 
-#add()
+
+def update(key,value):
+    find_key = input("What key do you want to change?: ")
+    if find_key in dictionary:
+        update_key = input("What would you like to change this key name to?: ")
+        update_value = input("What would you like to change this value name to?: ")
+        dictionary[key] = update_key
+        dictionary[value] = update_value
+    else:
+        print ("This key is not in your dictionary.")
+        update(key,value)
+
+
+
+def print():
+    pprint (dictionary)
+
+#def dictionary():
+
+add()
 remove_key()
